a3ccfcc3053c860e7fbe538c1fd74e9f457f76a3
dodemabiteniwe <dodemabiteniwe@gmail.com>
2024-02-18 19:02:13 -0500

    changes made from theia lab environment

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios')
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -20,41 +21,53 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
+const apiPath = 'https://dodemabiteni-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/';
+
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', function (req, res) {
   //Write your code here
- res.send(JSON.stringify(books,null,4));
+  let data = Promise.resolve(JSON.stringify(books,null,4));
+  data.then((data)=> res.send(data))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
- const isbn = req.params.isbn;
-  res.send(books[isbn])
+  let data = new Promise((resolve, reject) => {
+  const isbn = req.params.isbn;
+  resolve(books[isbn])
+  });
+  data.then((data)=> res.send(data))
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    const filteredObject = Object.keys(books).reduce((result, key) => {
-        if (books[key].author === author) {
-          result[key] = books[key];
-        }
-        return result;
-      }, {});
-    res.send(JSON.stringify(filteredObject,null,4));
+    let data = new Promise((resolve, reject) => {
+        const author = req.params.author;
+        const filteredObject = Object.keys(books).reduce((result, key) => {
+            if (books[key].author === author) {
+              result[key] = books[key];
+            }
+            return result;
+          }, {});
+          resolve(JSON.stringify(filteredObject,null,4))   
+    });
+    data.then((data)=> res.send(data));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    const filtered_title = Object.keys(books).reduce((result, key) => {
-        if (books[key].title === title) {
-          result[key] = books[key];
-        }
-        return result;
-      }, {});
-    res.send(JSON.stringify(filtered_title,null,4));
+    let data = new Promise((resolve, reject) => {
+        const title = req.params.title;
+        const filtered_title = Object.keys(books).reduce((result, key) => {
+            if (books[key].title === title) {
+              result[key] = books[key];
+            }
+            return result;
+          }, {});
+          resolve(JSON.stringify(filtered_title,null,4))   
+    });
+    data.then((data)=> res.send(data));;
 });
 
 //  Get book review
